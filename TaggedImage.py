99cadaba4f50228f216e00953a0818ea49c039ed
Sarvagya Gupta <guptasarvagyav@gmail.com>
2019-11-25 20:06:15 -0800

    Data loading done

--- TaggedImage.py
+++ TaggedImage.py
@@ -1,14 +1,15 @@
 """
-
+TaggedImage is representation of one image that represents an emotion
 """
 
 import numpy as np
+
 
 class TaggedImage:
 
     def __init__(self, csv_row):
         self.label = csv_row['emotion']
-        self.pixels = csv_row['pixels']
+        self.pixels = self.reshape_pixels(csv_row['pixels'])
 
-    def reshape_pixels(self):
-        self.pixels = np.array(self.pixels.split())
+    def reshape_pixels(self, pixels):
+        return np.array(pixels.split()).reshape(48, 48)
