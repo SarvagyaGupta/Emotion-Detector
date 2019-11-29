0e1f7f0e8d8d0bac593f8c9e607e82e2067db4a3
Sarvagya Gupta <guptasarvagyav@gmail.com>
2019-11-29 01:20:49 -0800

    API updated to kinda make everything work

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from ImageEmotionPredictor import EmotionDetector
-import cv2
-import json
+import werkzeug
 
 app = Flask(__name__)
 
@@ -10,12 +9,12 @@ detector = EmotionDetector()
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    r = request
-    predictions = detector.predict_emotion(r.data)
+    image_file = request.files['image']
+    print image_file
+    print type(image_file)
 
-    response = json.dumps(predictions)
-    return response
+    filename = werkzeug.utils.secure_filename(image_file.filename)
+    image_file.save(filename)
 
+    return detector.predict_emotion("123")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
