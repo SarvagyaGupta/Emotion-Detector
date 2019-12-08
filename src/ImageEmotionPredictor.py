08719a38863f7ee48709e30c7cb754f303afca50
Sarvagya <sarvagya@emotion-detector-v2.us-west2-a.c.calcium-task-260400.internal>
2019-12-08 03:30:35 +0000

    Flask changed to FastAPI. ImagePredictor changed to accomodate the API change

--- src/ImageEmotionPredictor.py
+++ src/ImageEmotionPredictor.py
@@ -7,6 +7,17 @@ import heapq
 import cv2
 import numpy as np
 from keras.models import model_from_json
+import base64
+import tensorflow as tf
+from tensorflow import keras
+
+
+config = tf.ConfigProto(
+    intra_op_parallelism_threads=1,
+    allow_soft_placement=True
+)
+session = tf.Session(config=config)
+keras.backend.set_session(session)
 
 
 class EmotionDetector:
@@ -36,31 +47,68 @@ class EmotionDetector:
 
         return predictions
 
-    def __predict_face_emotion(self, gray, face):
+    def predict_live_emotion(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        face_classifier = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
+        faces = face_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        for face in faces:
+            self.__predict_live_emotion(image, gray, face)
+
+        _, output_image = cv2.imencode('.jpg', image)
+        output_image = base64.b64encode(output_image)
+        return {'image': output_image}
+
+
+    def __predict_live_emotion(self, image, gray, face):
         x, y, w, h = face
         roi_gray = gray[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
         cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
 
-        # predict the probability
-        probability = self.modeled_emotion.predict_proba(cropped_img)
-        probab = []
-        for i in probability[0]:
-            probab.append(float('{:f}'.format(i)))
+        with session.as_default():
+            with session.graph.as_default():
+ 
+                prediction = self.modeled_emotion.predict(cropped_img)
+                predicted_emotion = self.labels[int(np.argmax(prediction))]
 
-        # index of the top 3 emotions
-        photo_indices = heapq.nlargest(3, range(len(probab)), probab.__getitem__)
+                self.draw_face_boundary(image, face, (0, 255, 0))
+                self.write_emotion(image, face, predicted_emotion, (0, 255, 0)) 
 
-        # percentage of that emotion
-        percentage = [100 * probab[index] for index in photo_indices]
-        percentage = [round(percent, 2) for percent in percentage]
 
-        # labels of those percentages
-        emotions = [self.labels[index] for index in photo_indices]
+    def __predict_face_emotion(self, gray, face):
+        x, y, w, h = face
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
+        
+        with session.as_default():
+            with session.graph.as_default():
+                # predict the probability
+                probability = self.modeled_emotion.predict_proba(cropped_img)
+                probab = []
+                for i in probability[0]:
+                    probab.append(float('{:f}'.format(i)))
 
-        return {'top_left_x': x.item(), 'top_left_y': y.item(), 'height': h.item(), 'width': w.item()
-                , 'emotions': emotions, 'emotion_percentages': percentage}
+                # index of the top 3 emotions
+                photo_indices = heapq.nlargest(3, range(len(probab)), probab.__getitem__)
+
+                # percentage of that emotion
+                percentage = [100 * probab[index] for index in photo_indices]
+                percentage = [round(percent, 2) for percent in percentage]
+
+                # labels of those percentages
+                emotions = [self.labels[index] for index in photo_indices]
+
+                return {'top_left_x': x.item(), 'top_left_y': y.item(), 'height': h.item(), 'width': w.item()
+                        , 'emotions': emotions, 'emotion_percentages': percentage}
+
 
     def draw_face_boundary(self, image, face, color):
         x, y, w, h = face
         cv2.rectangle(image, pt1=(x, y), pt2=(x + w, y + h), color=color, thickness=2)
+
+
+    def write_emotion(self, image, face, emotion, color):
+        x, y, w, h = face
+        cv2.putText(image, emotion, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, color, 2, cv2.LINE_AA)
--- src/app.py
+++ src/app.py
@@ -1,20 +1,24 @@
-from flask import Flask, request
+from fastapi import FastAPI
+from pydantic import BaseModel
+from flask import Flask
 from ImageEmotionPredictor import EmotionDetector
 import numpy as np
 import cv2
 import base64
 
-app = Flask(__name__)
 
+class Image(BaseModel):
+    encoded_image: str
+
+#app = Flask(__name__)
+app = FastAPI()
 detector = EmotionDetector()
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    input_image = base64.b64decode(request.data)
-    input_image = np.asarray(bytearray(input_image), dtype='uint8')
-    input_image = cv2.imdecode(input_image, cv2.IMREAD_COLOR)
-
+@app.post('/predict/image')
+def predict_image(image: Image):
+    print (image)
+    input_image = get_input_image(image.encoded_image)
     predictions = detector.predict_emotion(input_image)
     for prediction in predictions['predictions']:
         detector.draw_face_boundary(input_image, (prediction['top_left_x'], prediction['top_left_y']
@@ -25,7 +29,17 @@ def predict():
 
     return predictions
 
+@app.post('/predict/live')
+def predict_live(image: Image):
+    input_image = get_input_image(image.encoded_image)
+    return detector.predict_live_emotion(input_image)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+def get_input_image(encoded_image):
+    input_image = base64.b64decode(encoded_image)
+    input_image = np.asarray(bytearray(input_image), dtype='uint8')
+    return cv2.imdecode(input_image, cv2.IMREAD_COLOR)
+
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, debug=True)
 
